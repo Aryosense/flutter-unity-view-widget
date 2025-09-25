87f5198ce5193ca9f5987fb06a6421c2d81a00a0
Tim H <timbotimbo@users.noreply.github.com>
2025-09-25 16:44:22 +0200

    Update SymbolDefineHelper for legacy Unity versions.

--- example/unity/DemoApp/Assets/FlutterUnityIntegration/Editor/SymbolDefineHelper.cs
+++ example/unity/DemoApp/Assets/FlutterUnityIntegration/Editor/SymbolDefineHelper.cs
@@ -1,24 +1,24 @@
 using System.Collections.Generic;
 using UnityEditor;
-using UnityEditor.Build;
 using UnityEngine;
+using UnityEditor.Build;
 
 namespace FlutterUnityIntegration.Editor
 {
-    public static class SymbolDefineHelper 
+    public static class SymbolDefineHelper
     {
         public static void SetScriptingDefine(string symbol, bool enable)
         {
             bool changesMade = false;
-
+            
             // Iterate over all named build targets available
+#if UNITY_2021_2_OR_NEWER
             foreach (NamedBuildTarget namedTarget in GetAllNamedBuildTargets())
             {
                 string defines = PlayerSettings.GetScriptingDefineSymbols(namedTarget);
                 var defineList = new List<string>(defines.Split(';'));
 
                 bool contains = defineList.Contains(symbol);
-
                 if (enable && !contains)
                 {
                     defineList.Add(symbol);
@@ -32,9 +32,31 @@ namespace FlutterUnityIntegration.Editor
                     Debug.Log($"Removed '{symbol}' from {namedTarget.TargetName}");
                 }
 
-                string updatedDefines = string.Join(";", defineList);
-                PlayerSettings.SetScriptingDefineSymbols(namedTarget, updatedDefines);
+                PlayerSettings.SetScriptingDefineSymbols(namedTarget, string.Join(";", defineList));
             }
+#else
+            foreach (BuildTargetGroup group in GetAllBuildTargetGroups())
+            {
+                string defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(group);
+                var defineList = new List<string>(defines.Split(';'));
+
+                bool contains = defineList.Contains(symbol);
+                if (enable && !contains)
+                {
+                    defineList.Add(symbol);
+                    changesMade = true;
+                    Debug.Log($"Added '{symbol}' to {group}");
+                }
+                else if (!enable && contains)
+                {
+                    defineList.Remove(symbol);
+                    changesMade = true;
+                    Debug.Log($"Removed '{symbol}' from {group}");
+                }
+
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(group, string.Join(";", defineList));
+            }
+#endif
 
             if (changesMade)
             {
@@ -47,6 +69,7 @@ namespace FlutterUnityIntegration.Editor
             }
         }
 
+#if UNITY_2021_2_OR_NEWER
         private static IEnumerable<NamedBuildTarget> GetAllNamedBuildTargets()
         {
             yield return NamedBuildTarget.FromBuildTargetGroup(BuildTargetGroup.Standalone);
@@ -54,5 +77,14 @@ namespace FlutterUnityIntegration.Editor
             yield return NamedBuildTarget.FromBuildTargetGroup(BuildTargetGroup.iOS);
             yield return NamedBuildTarget.FromBuildTargetGroup(BuildTargetGroup.WebGL);
         }
+#else
+        private static IEnumerable<BuildTargetGroup> GetAllBuildTargetGroups()
+        {
+            yield return BuildTargetGroup.Standalone;
+            yield return BuildTargetGroup.Android;
+            yield return BuildTargetGroup.iOS;
+            yield return BuildTargetGroup.WebGL;
+        }
+#endif
     }
 }
